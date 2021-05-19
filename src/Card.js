3dade168226fde75b7ecae701c41ab14e77ead93
andrewneilclarke <andrewneilclarke@gmail.com>
2021-05-19 16:19:47 +0200

    added toggle feature

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ export const Card = ( { stories, onToggle } ) => {
                     return (
                         <div key={id} className="card" onDoubleClick={() => onToggle(id)}>
                             <h1 key={title} className="title">{title}</h1> 
-                            <p key={story.id} className="text">{story.open ? {text} : null }</p>
+                            <p key={story.id} className="text">{story.open ? text : null }</p>
                         </div>
                       );
                 })}
