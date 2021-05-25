f27f595468c16f94ccbb0f5e4d38b9c485657925
andrewneilclarke <andrewneilclarke@gmail.com>
2021-05-25 17:02:04 +0200

    added image

--- src/Card.js
+++ src/Card.js
@@ -9,7 +9,7 @@ export const Card = ( { stories, onToggle, onDelete } ) => {
                         <div key={id} className="card" onDoubleClick={() => onToggle(id)}>
                             <h1 key={title} className="title">{title} <FaTimesCircle style={{ paddingLeft: '60px', cursor: 'pointer', fontSize: 'xx-large', color: 'purple', opacity: '0.15' }} onClick={() => onDelete(id)}/> </h1> 
                             
-                            <p key={story.id} className="text">{story.open ? [ <img src={image} />, text ] : null }</p>
+                            <p key={story.id} className="text">{story.open ? [ <img src={image} alt="" />, text ] : null }</p>
                         </div>
                       );
                 })}
