0880a0e8cc84cb01d558ac4f19c8edcb2ba8215d
andrewneilclarke <andrewneilclarke@gmail.com>
2021-05-25 16:58:32 +0200

    added image

--- src/Card.js
+++ src/Card.js
@@ -4,11 +4,12 @@ export const Card = ( { stories, onToggle, onDelete } ) => {
     return (
         <div>
                 {stories.map((story) => {
-                    const { id, title, text } = story;
+                    const { id, title, text, image } = story;
                     return (
                         <div key={id} className="card" onDoubleClick={() => onToggle(id)}>
                             <h1 key={title} className="title">{title} <FaTimesCircle style={{ paddingLeft: '60px', cursor: 'pointer', fontSize: 'xx-large', color: 'purple', opacity: '0.15' }} onClick={() => onDelete(id)}/> </h1> 
-                            <p key={story.id} className="text">{story.open ? text : null }</p>
+                            
+                            <p key={story.id} className="text">{story.open ? [ <img src={image} />, text ] : null }</p>
                         </div>
                       );
                 })}
