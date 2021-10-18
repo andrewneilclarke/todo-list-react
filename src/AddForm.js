dc8f1604998cbda05560fc093a50eac4ba5f5c45
andrewneilclarke <andrewneilclarke@gmail.com>
2021-10-18 17:40:50 +0200

    changed design and scope

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-  
 
-const AddForm = ( { onAdd, stories } ) => {
-    
+
+const AddForm = ({ onAdd, stories }) => {
+
     const [title, setTitle] = useState('')
     const [text, setStory] = useState('')
     const [newId, setNewId] = useState(2400)
@@ -11,11 +11,11 @@ const AddForm = ( { onAdd, stories } ) => {
     const onSubmit = (e) => {
         e.preventDefault();
         const id = Math.floor(Math.random() * 10000) + 1;
-        if(!text) {
+        if (!text) {
             alert('Please add a story')
             return
         }
-        onAdd( { title, text, newId, open } )
+        onAdd({ title, text, newId, open })
         setTitle('')
         setStory('')
         setNewId(id)
@@ -24,15 +24,11 @@ const AddForm = ( { onAdd, stories } ) => {
     return (
         <form className="add-form" onSubmit={onSubmit}>
             <div className="form-control">
-                <label>Title</label>
-                <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                <input type="text" placeholder="Add a task" value={title} onChange={(e) => setTitle(e.target.value)} />
             </div>
-            <div className="form-control">
-                <label>Story</label>
-                <input type="text" placeholder="Story" value={text} onChange={(e) => setStory(e.target.value)}/>
-            </div>
+
             <input type="submit" className={"btn btn-block"} value="Save" />
-               
+
         </form>
     )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import Button from './Button'
 
-export const Header = ( {onAdd, showAdd} ) => {
+export const Header = ({ onAdd, showAdd }) => {
     return (
         <header className="header">
-            <h4>Bedtime Stories</h4>
-            <Button 
-            color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+            <h4>My Todo List</h4>
+            <Button
+                color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add todo'} onClick={onAdd} />
         </header>
     )
 }
