5e6f444129d986df0fa7a026aca9d1b29e6835fb
andrewneilclarke <andrewneilclarke@gmail.com>
2021-10-31 14:48:18 +0100

    fixed css issue

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,13 @@
 import { FaTimesCircle } from 'react-icons/fa'
 
-export const Card = ({ tasks, onToggle, onDelete }) => {
+export const Card = ({ tasks, onDelete }) => {
     return (
         <div>
             {tasks.map((task) => {
                 const { id, title } = task;
                 return (
-                    <div key={id} className="card" onDoubleClick={() => onToggle(id)}>
-                        <h1 key={title} className="title">{title} <FaTimesCircle style={{ paddingLeft: '60px', cursor: 'pointer', fontSize: 'xx-large', color: 'var(--color-green-text)', opacity: '0.15' }} onClick={() => onDelete(id)} /> </h1>
+                    <div key={id} className="card">
+                        <h1 key={title} className="title">{title} <FaTimesCircle style={{ paddingLeft: '10px', cursor: 'pointer', fontSize: 'xx-large', color: 'var(--color-green-text)', opacity: '0.25' }} onClick={() => onDelete(id)} /> </h1>
                     </div>
                 );
             })}
